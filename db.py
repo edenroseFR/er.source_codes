88e71e0922dad562ff859c1bd0434be6df1f45b8
Eden Rose <edenroserisma13@gmail.com>
2021-06-01 18:31:26 +0800

    form modified

--- db.py
+++ db.py
@@ -5,3 +5,56 @@ database = mysql.connect(host = 'localhost',
                                  password = 'edenrose',
                                  database = 'student_information')
 cursor = database.cursor()
+
+def students():
+    query = 'SELECT * FROM students'
+    cursor.execute(query)
+    students = cursor.fetchall()
+    students = [list(student) for student in students]
+
+    for i in range(len(students)):
+        if students[i][2] != '':
+            students[i] = [students[i][0], students[i][1] + ' '+ students[i][2][0] + ' ' + students[i][3], students[i][4], students[i][5], students[i][6]]
+        else:
+            students[i] = [students[i][0], students[i][1] + ' '+ students[i][2] + ' ' + students[i][3], students[i][4], students[i][5], students[i][6]]
+
+    return students
+
+def get_student(query=None):
+    cursor.execute(query)
+    student = cursor.fetchall()
+    student = list(student[0])
+
+    return student
+
+
+def get_courses(query='SELECT course_code from courses'):
+    cursor.execute(query)
+    courses = cursor.fetchall()
+    courses = [list(i) for i in courses]
+
+    for i in range(len(courses)):
+        courses[i] = courses[i][0]
+
+    return courses
+
+
+def update_student(id, first, middle, last, course, year, gender):
+    query = ('UPDATE students SET first_name = "%s", middle_name = "%s", last_name = "%s", fk_course_code = "%s", year_level = "%s", gender = "%s" WHERE students_id = "%s";' % ( first, middle, last, course, year, gender, id ))
+    cursor.execute(query)
+    database.commit()
+    return
+
+
+def delete_student(id):
+    query = ('DELETE FROM students WHERE students_id  = "%s";' % id)
+    cursor.execute(query)
+    database.commit()
+    return
+
+def add_student(id, first, middle, last, course, year, gender):
+    query = ('INSERT INTO students(students_id, first_name, middle_name, last_name, fk_course_code, year_level, gender) VALUES("%s", "%s","%s","%s","%s","%s","%s")'%(id,first,middle,last,course,year,gender))
+    cursor.execute(query)
+    database.commit()
+    return
+
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import sys
-import db
+
+from PyQt5.QtWidgets import QTableWidgetItem
+
 import re
 from PyQt5 import QtCore, QtGui, QtWidgets
 from ssis_main_UI import Ui_MainWindow
 from ssis_login_UI import Ui_Login
 from ssis_signup_UI import Ui_SignUp
-from addStudent import Ui_Form
+from form import Ui_Form
+import db
 
 class Login(QtWidgets.QWidget, Ui_Login):
     def __init__(self):
@@ -18,6 +21,7 @@ class Login(QtWidgets.QWidget, Ui_Login):
     def configureWidgets(self):
         self.ui.pushButton_login.clicked.connect(self.checkCredentials)
         self.ui.pushButton_signup.clicked.connect(self.goto_SignUpWindow)
+
     def checkCredentials(self):
         entered_username = self.ui.lineEdit_username.text()
         entered_password = self.ui.lineEdit_password.text()
@@ -95,43 +99,57 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def configureWidgets(self):
         self.ui.pushButton_addNew.clicked.connect(self.goto_addStudent)
+        self.fillTable()
+
+    def fillTable(self):
+        students = db.students()
+        row = 0
+        self.ui.tableWidget.setRowCount(len(students))
+        for student in students:
+            self.ui.tableWidget.setItem(row, 0, QTableWidgetItem(student[0]))
+            self.ui.tableWidget.setItem(row, 1, QTableWidgetItem(student[1]))
+            self.ui.tableWidget.setItem(row, 2, QTableWidgetItem(student[2]))
+            self.ui.tableWidget.setItem(row, 3, QTableWidgetItem(str(student[3])))
+            self.ui.tableWidget.setItem(row, 4, QTableWidgetItem(str(student[4])))
+
+            row += 1
+        return
 
     def goto_addStudent(self):
-        self.popUp = AddStudent()
+        self.popUp = AddStudent(self)
         self.popUp.show()
 
 
-class AddStudent(QtWidgets.QWidget, Ui_Form):
-    def __init__(self):
-        super().__init__()
+class AddStudent(QtWidgets.QMainWindow, Ui_Form):
+    def __init__(self, parent=None):
+        super(AddStudent, self).__init__(parent)
+        self.p = parent
         self.ui = Ui_Form()
         self.ui.setupUi(self)
         self.configureWidgets()
 
     def configureWidgets(self):
-        self.ui.pushButton_AddStudent.clicked.connect(self.checkInformation)
+        self.ui.pushButton.clicked.connect(self.checkInformation)
+        self.ui.comboBox.addItems(db.get_courses())
+        self.ui.comboBox_2.addItems(['1','2','3','4','5','6'])
+        self.ui.comboBox_3.addItems(['Male','Female','Lesbian','Gay','Bisexual','Transgender','Unknown'])
 
     def checkInformation(self):
-        id = self.ui.studentIDLineEdit.text()
-        first_name = self.ui.firstNameLineEdit.text()
-        middle_name = self.ui.lastNameLineEdit.text()
-        last_name = self.ui.lastNameLineEdit.text()
-        course = self.ui.courseLineEdit.text()
-        year = str(self.ui.yearLevelComboBox.currentText())
-        gender = str(self.ui.genderComboBox.currentText())
-
-        if id and first_name and middle_name and last_name and course and year and gender != 'Choose Gender':
-            print('complete')
+        id = self.ui.lineEdit.text()
+        first_name = self.ui.lineEdit_2.text()
+        middle_name = self.ui.lineEdit_3.text()
+        last_name = self.ui.lineEdit_6.text()
+        course = self.ui.comboBox.currentText()
+        year = str(self.ui.comboBox_2.currentText())
+        gender = str(self.ui.comboBox_3.currentText())
+        if id and first_name and middle_name and last_name and course and year and gender:
+            db.add_student(id,first_name,middle_name,last_name,course,year,gender)
+            self.p.fillTable()
 
 
 
 
 app = QtWidgets.QApplication(sys.argv)
-win = Login()
+win = MainWindow()
 win.show()
 sys.exit(app.exec_())
-
-
-
-
-
